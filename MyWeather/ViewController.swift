6e241e519a25c1349cc77d7a2f3ef3a8b6e3c31a
Roman Khovalko <hromnik@me.com>
2024-11-28 14:55:45 +0200

    Initial Commit

---
+++ MyWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyWeather
+//
+//  Created by iHRomnik on 28.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MyWeatherTests/MyWeatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyWeatherTests.swift
+//  MyWeatherTests
+//
+//  Created by iHRomnik on 28.11.2024.
+//
+
+import Testing
+@testable import MyWeather
+
+struct MyWeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
